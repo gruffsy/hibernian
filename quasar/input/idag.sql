944258412254eb790cf6db2ea9729d1882979f17
una <una@hibernian.local>
2020-09-03 10:33:14 +0200

    made changes

--- quasar/input/idag.sql
+++ quasar/input/idag.sql
@@ -3,12 +3,11 @@ set
 set
     noexec on
 SET
-    LANGUAGE Norwegian;
+    LANGUAGE Norwegian
 set
     noexec off
 
 	
-
 select
 	s.Name as 'Butikk',
 	FORMAT(sum(tl.extendedprice),'### ### ##0 kr') AS 'Beløp m/mva',
@@ -33,26 +32,13 @@ where
 	s.ERPHostCompanyNo,
 	s.WarehouseNo,
 	s.Name
+order by
+	
+	s.ERPHostCompanyNo,
+	s.WarehouseNo,
+	s.Name
 	
 	
-	union all
-	select
-	'Totalt',
-	FORMAT(sum(tl.extendedprice),'### ### ##0 kr') AS 'Beløp m/mva',
-	FORMAT(sum((tl.extendedprice) / 1.25),'### ### ##0 kr') AS 'Beløp u/mva',
-	FORMAT(sum(((tl.extendedprice) / 1.25) - (tl.costprice * tl.quantity)),'### ### ##0 kr') AS 'DB',
-	FORMAT(sum(((tl.extendedprice) / 1.25) - (tl.costprice * tl.quantity))/sum(tl.extendedprice / 1.25), 'P') as 'DG',
-	FORMAT(count(distinct tr.PosTransactionNo), '### ### ##0') as 'Antall ordre',
-	FORMAT(sum(tl.extendedprice)/count(distinct tr.PosTransactionNo), '### ### ##0 kr') as 'per kunde'
 	
-from
-	ErpPosDb.dbo.vcrPosTransactionLine tl,
-	ErpPosDb.dbo.vcrPosTransaction tr,
-	ErpPosDb.dbo.vcrStore s
-where
-	s.StoreNo = tr.StoreNo and 
-	tl.PosTransactionNo = tr.PosTransactionNo 
-	and cast(tr.CompletedDate as DATE) = cast(GETDATE() as DATE)
-	and tr.Status = 4
 	
 for json auto
