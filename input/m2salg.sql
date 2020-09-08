46017d1dbbbe112859ff0d6b59fe74d02bfb6b12
una <una@hibernian.local>
2020-09-08 10:21:13 +0200

    made changes

---
+++ input/m2salg.sql
@@ -0,0 +1,32 @@
+set
+    nocount on
+set
+    noexec on
+SET
+    LANGUAGE Norwegian
+set
+    noexec off
+
+	
+select
+	tl.ProductNo,
+	tl.ProductName,
+	sum(tl.quantity)
+	
+from
+	ErpPosDb.dbo.vcrPosTransactionLine tl,
+	ErpPosDb.dbo.vcrPosTransaction tr,
+	ErpPosDb.dbo.vcrStore s
+where
+	s.StoreNo = tr.StoreNo and 
+	tl.PosTransactionNo = tr.PosTransactionNo 
+	and cast(tr.CompletedDate as DATE) > '2020-01-01'
+	and tr.Status = 4
+	and tl.UnitNo = 2
+	
+	group by
+	tl.ProductNo,
+	tl.ProductName
+	order by 
+	tl.ProductNo
+for json auto
