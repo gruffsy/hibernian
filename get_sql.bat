62eeb16fb17c1c7554064d91a162d3e79370d4cb
una <una@hibernian.local>
2020-10-16 12:06:36 +0200

    made changes

--- get_sql.bat
+++ get_sql.bat
@@ -3,7 +3,8 @@ SET user=sa
 SET password=VismaVudAdmin123@
 SET outputpath=D:\Scripts\Salgstall\publish\json\
 SET inputpath=D:\Scripts\Salgstall\publish\input\
-
+SET csvoutputpath=D:\Scripts\Salgstall\publish\lagersaldo\
+SET csvinputpath=D:\Scripts\Salgstall\publish\inputlagersaldo\
 
 
 rem @ECHO %%~nxI
@@ -12,4 +13,6 @@ FOR %%I IN (%inputpath%*) DO (
 sqlcmd -S %server% -U %user% -P %password% -i "%inputpath%%%~nxI" -y0 -o "%outputpath%%%~nxI.json" -f i:65001 -f o:65001
 )
 
+
+call sqlcmd -S %server% -U %user% -P %password% -s ";" -W -i %csvinputpath%lagersaldo.sql | findstr /v /c:"-" /b > %csvoutputpath%lagersaldo.csv
 call git_push.bat
---
+++ inputlagersaldo/lagersaldo.sql
@@ -0,0 +1,65 @@
+set
+    nocount on
+set
+    noexec on
+SET
+    LANGUAGE Norwegian
+set
+    noexec off
+
+	
+ /****** Script for SelectTopNRows command from SSMS  ******/
+select 
+	prodno as 'Produktnummer',
+	trinf1 as 'Lev.prono',
+	eanitmNo as 'Strekkode',
+	descr as 'Beskrivelse',
+	inf as 'Sortimentskode',
+
+( SELECT 
+      sum(bal + stcinc - shprsv - shprsvin)
+FROM [F0001].[dbo].[StcBal] as ski
+where stcno = 1 and ski.prodno = p.prodno
+) as 'Skien'
+,( SELECT 
+      sum(bal + stcinc - shprsv - shprsvin)
+FROM [F0001].[dbo].[StcBal] as por
+where stcno = 2 and por.prodno = p.prodno
+) as 'Porsgrunn'
+,( SELECT 
+      sum(bal + stcinc - shprsv - shprsvin)
+FROM [F0002].[dbo].[StcBal] as kris
+where stcno = 1 and kris.prodno = p.prodno
+) as 'Kristiansand'
+,( SELECT 
+      sum(bal + stcinc - shprsv - shprsvin)
+FROM [F0003].[dbo].[StcBal] as ton
+where stcno = 1 and ton.prodno = p.prodno
+) as 'Tønsberg'
+,
+( SELECT 
+      sum(bal + stcinc - shprsv - shprsvin)
+FROM [F0004].[dbo].[StcBal] as are
+where stcno = 1 and are.prodno = p.prodno
+) as 'Arendal'
+,
+( SELECT 
+      sum(bal + stcinc - shprsv - shprsvin)
+FROM [F0010].[dbo].[StcBal] as san
+where stcno = 1 and san.prodno = p.prodno
+) as 'Sandefjord'
+,
+( SELECT 
+      sum(bal + stcinc - shprsv - shprsvin)
+FROM [F0016].[dbo].[StcBal] as lar
+where stcno = 1 and lar.prodno = p.prodno
+) as 'Larvik',
+( SELECT 
+      sum(bal + stcinc - shprsv - shprsvin)
+FROM [F0015].[dbo].[StcBal] as stock
+where stcno = 1 and stock.prodno = p.prodno
+) as 'Stock'
+from [F0001].[dbo].[prod] as p
+where prodpro <> 4
+go
+    quit
