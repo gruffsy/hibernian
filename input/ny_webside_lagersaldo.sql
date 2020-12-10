5dceb7951f2f90376f4700f6f83c77c071e9bcbf
una <una@hibernian.local>
2020-12-10 14:42:00 +0100

    made changes

---
+++ input/ny_webside_lagersaldo.sql
@@ -0,0 +1,30 @@
+﻿set
+    nocount on
+set
+    noexec on
+SET
+    LANGUAGE Norwegian
+set
+    noexec off
+
+
+USE F0001;
+SELECT  
+	CONVERT(varchar(13), p.EANItmNo) as SkuNumber,
+	CONVERT(decimal(25,4), l.Bal + l.StcInc - l.ShpRsv - l.ShpRsvIn) as Quantity,
+	CONVERT(varchar(4), 'S140') as Warehouse 
+FROM
+	StcBal as l,
+	Prod as p
+WHERE
+	l.StcNo = 1 AND
+	l.ProdNo = p.ProdNo AND
+	p.EANItmNo <> '' AND
+	(
+	l.prodno = '314128' OR 
+	l.prodno = 'GB7199H8BN160' OR
+	l.prodno = 'LS3547-OAK'
+	)
+FOR JSON AUTO;
+go
+    quit
