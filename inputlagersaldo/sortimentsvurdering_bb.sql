ee0ce51a39ccba7f0bac4dfc0bfbb505374d9b35
una <una@hibernian.local>
2021-10-29 13:30:43 +0200

    made changes

---
+++ inputlagersaldo/sortimentsvurdering_bb.sql
@@ -0,0 +1,36 @@
+﻿set
+    nocount on
+set
+    noexec on
+SET
+    LANGUAGE Norwegian
+set
+    noexec off
+
+SELECT 
+	
+	DISTINCT FORMAT ( CONVERT(datetime, convert(varchar(10), Fakturadato)), 'yyyy') AS ar,
+	NAVnr as 'ItemNo',
+	SUM(Antall) as 'SalesQty',
+	SUM(u_mva) AS 'SalesAmountExclVat',
+	SUM(m_mva) AS 'SalesAmountInclVat',
+	SUM(DB) AS 'BF'
+  FROM 
+  [F0001].[dbo].[varmepumper_ALLE]
+  WHERE 
+	FORMAT ( CONVERT(datetime, convert(varchar(10), Fakturadato)), 'yyyy') > 2019
+AND
+  Fakturadato <> 0
+  AND 
+  Transaksjonstype = 1
+  AND 
+  Kundeprisgruppe <> 4
+  AND
+  NAVnr <> ''
+  
+  
+  GROUP BY
+  	Strekkode,
+	NAVnr
+go
+    quit
