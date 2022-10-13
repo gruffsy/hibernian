b00eab0395fe07cff66e33cfba28e7c4e67c84c5
gruffsy <peamoen@gmail.com>
2022-10-13 12:09:15 +0200

    made changes

--- get_sql.bat
+++ get_sql.bat
@@ -1,3 +1,15 @@
+
+
+rem SET server=10.0.10.41
+rem SET user=intranett
+rem SET password=Megareader18
+rem SET vpnpath="c:\program files\SonicWall\Global VPN Client\"
+rem SET sqlpath=S:\Unified_SQL\
+rem %vpnpath%swgvc /E "Billingstad" /U "per" /P "MegaPass###"
+rem timeout /t 10
+
+
+
 SET server=DB-HIB
 SET user=sa
 SET password=VismaVudAdmin123@
@@ -6,20 +18,13 @@ SET inputpath=D:\Scripts\Salgstall\publish\input\
 SET csvoutputpath=D:\Scripts\Salgstall\publish\lagersaldo\
 SET csvinputpath=D:\Scripts\Salgstall\publish\inputlagersaldo\
 
-
 rem @ECHO %%~nxI
 
 FOR %%I IN (%inputpath%*) DO (
 sqlcmd -S %server% -U %user% -P %password% -i "%inputpath%%%~nxI" -y0 -o "%outputpath%%%~nxI.json" -f i:65001 -f o:65001
 )
 
-SET server=10.0.10.41
-SET user=intranett
-SET password=Megareader18
-SET vpnpath="c:\program files\SonicWall\Global VPN Client\"
-SET sqlpath=S:\Unified_SQL\
-%vpnpath%swgvc /E "Billingstad" /U "per" /P "MegaPass###"
-timeout /t 10
+
 
 
 
