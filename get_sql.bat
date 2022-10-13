406e15f445aad85fb934f8a7e1af27ecca706d10
gruffsy <peamoen@gmail.com>
2022-10-13 12:03:13 +0200

    made changes

--- get_sql.bat
+++ get_sql.bat
@@ -13,6 +13,16 @@ FOR %%I IN (%inputpath%*) DO (
 sqlcmd -S %server% -U %user% -P %password% -i "%inputpath%%%~nxI" -y0 -o "%outputpath%%%~nxI.json" -f i:65001 -f o:65001
 )
 
+SET server=10.0.10.41
+SET user=intranett
+SET password=Megareader18
+SET vpnpath="c:\program files\SonicWall\Global VPN Client\"
+SET sqlpath=S:\Unified_SQL\
+%vpnpath%swgvc /E "Billingstad" /U "per" /P "MegaPass###"
+timeout /t 10
+
+
+
 
 call sqlcmd -S %server% -U %user% -P %password% -s ";" -W -i %csvinputpath%lagersaldo.sql | findstr /v /c:"-" /b > %csvoutputpath%lagersaldo.csv
 
