4763297e16ef4516d376a9f883c4a1f83f0e73c0
gruffsy <peamoen@gmail.com>
2022-10-18 15:25:13 +0200

    made changes

--- get_sql.bat
+++ get_sql.bat
@@ -14,7 +14,7 @@ sqlcmd -S %server% -U %user% -P %password% -i "%inputpath%%%~nxI" -y0 -o "%outpu
 
 rem call sqlcmd -S %server% -U %user% -P %password% -s ";" -W -i %csvinputpath%lagersaldo.sql | findstr /v /c:"-" /b > %csvoutputpath%lagersaldo.csv
 
-call git_push.bat
+rem call git_push.bat
 
 
 SET server=10.0.10.41
@@ -22,14 +22,14 @@ SET user=intranett
 SET password=Megareader18
 SET vpnpath="c:\program files\SonicWall\Global VPN Client\"
 SET sqlpath=S:\Unified_SQL\
-%vpnpath%swgvc /E "Billingstad" /U "per" /P "MegaPass###"
+call %vpnpath%swgvc /E "Billingstad" /U "per" /P "MegaPass###"
 timeout /t 10
 
-call sqlcmd -S %server% -U %user% -P %password% -i salg.sql -y0 -s ";" -o salg.json -f i:65001 -f o:65001
+call sqlcmd -S %server% -U %user% -P %password% -i salg.sql -y0 -s ";" -o salg.json
 
 rem Powershell.exe -executionpolicy remotesigned -File .\merge.ps1
 
 
-rem %vpnpath%swgvc /D "Billingstad"
+call %vpnpath%swgvc /D "Billingstad"
 
 call git_push.bat
