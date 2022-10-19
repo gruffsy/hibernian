35c059a30b749ee1d36d33f0e81db465e8f509fb
gruffsy <peamoen@gmail.com>
2022-10-19 12:31:27 +0200

    made changes

--- salg.sql
+++ salg.sql
@@ -14,7 +14,7 @@ select
     	FORMAT(sum(se.[Net Amount])*-1, '### ### ##0 kr') as umoms,
     	FORMAT(sum(se.[Net Amount])*-1-sum(se.[Cost Amount])*-1, '### ### ##0 kr') as db,
 	FORMAT(sum(se.[Net Amount]-se.[Cost Amount])/sum(se.[Net Amount]), 'P1') as dg,
-	count(th.[Receipt No_]) as antord,
+	count(distinct th.[Receipt No_]) as antord,
     FORMAT(sum(se.[Net Amount]-se.[Cost Amount])/sum(se.[Net Amount]), 'P1') as prord
 
 
@@ -30,7 +30,7 @@ INNER JOIN [Megaflis Bamble AS$Store] s
 on
 	s.No_=th.[Store No_]
 where 	th.[Transaction Type]=2 
-       	and [Entry Status] in (0,2)
+       	and th.[Entry Status] in (0,2)
 	and th.[Date] >= convert(varchar, getdate()-5, 112)
 	and nullif(th.[Receipt No_],'') is not null
 group by
