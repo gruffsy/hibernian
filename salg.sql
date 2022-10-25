4d183ca473c38bb22397f80707e8e89b02e6eb88
gruffsy <peamoen@gmail.com>
2022-10-25 11:16:48 +0200

    made changes

--- salg.sql
+++ salg.sql
@@ -10,34 +10,31 @@ set
 select
 	datename(WEEKDAY, th.[Date]) as butikk, 
 	7 as Klient,
-      	FORMAT(sum([Total Rounded Amt_])*-1, '### ### ##0 kr') as mmoms,
-    	FORMAT(sum(se.[Net Amount])*-1, '### ### ##0 kr') as umoms,
-    	FORMAT(sum(se.[Net Amount])*-1-sum(se.[Cost Amount])*-1, '### ### ##0 kr') as db,
+    FORMAT(sum([Total Rounded Amt_])*-1, '### ### ##0 kr') as mmoms,
+    FORMAT(sum(se.[Net Amount])*-1, '### ### ##0 kr') as umoms,
+    FORMAT(sum(se.[Net Amount])*-1-sum(se.[Cost Amount])*-1, '### ### ##0 kr') as db,
 	FORMAT(sum(se.[Net Amount]-se.[Cost Amount])/sum(se.[Net Amount]), 'P1') as dg,
 	count(distinct th.[Receipt No_]) as antord,
     FORMAT(sum(-[Total Rounded Amt_])/count(distinct th.[Receipt No_]), '### ### ##0 kr') as prord
-	
-
-
-
 from
       	[Megaflis Bamble AS$Trans_ Sales Entry] se 
 inner join [Megaflis Bamble AS$Transaction Header] th 
 on 
 	th.[Store No_]=se.[Store No_] and 
-       	th.[POS Terminal No_]=se.[POS Terminal No_] and 
-       	th.[Transaction No_]=se.[Transaction No_] 
+   	th.[POS Terminal No_]=se.[POS Terminal No_] and 
+   	th.[Transaction No_]=se.[Transaction No_] 
 INNER JOIN [Megaflis Bamble AS$Store] s 
 on
 	s.No_=th.[Store No_]
 where 	th.[Transaction Type]=2 
-       	and th.[Entry Status] in (0,2)
+	and th.[Entry Status] in (0,2)
 	and th.[Date] >= convert(varchar, getdate()-5, 112)
 	and nullif(th.[Receipt No_],'') is not null
+	and [Customer Account] = 0
 group by
-th.[Date]
+	th.[Date]
 order by
-th.[Date]
+	th.[Date]
 for json auto
 go
 quit
