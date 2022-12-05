341ac7a81eb3d3b1901528035fa3a19861ecb6c8
gruffsy <peamoen@gmail.com>
2022-12-05 15:46:51 +0100

    made changes

--- input/lager_stock.sql
+++ input/lager_stock.sql
@@ -13,6 +13,7 @@ SELECT
     ,CAST(sum(s.bal + s.stcinc - ino + cino) as decimal(7,1)) as 'm2'
 	,CAST(p.Free1 AS DECIMAL(7,1)) as 'Prpall'
 	,CAST((sum(s.bal + s.stcinc - ino + cino)/NULLIF(p.free1, 0)) as DECIMAL(7,0)) as  'Paller'
+    ,CAST(sum(s.InPurc/NULLIF(p.free1, 0)) as DECIMAL(7,0)) as 'Paller på vei'
   FROM F0015.dbo.StcBal s, f0015.dbo.prod p
   where 
   s.prodno = p.prodno
