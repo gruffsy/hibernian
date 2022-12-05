5bd281977f0098d5e6618cfee1311c387503732b
gruffsy <peamoen@gmail.com>
2022-12-05 14:16:46 +0100

    made changes

--- input/lager_stock.sql
+++ input/lager_stock.sql
@@ -8,17 +8,22 @@ set
     noexec off
 
 SELECT 
-	s.ProdNo as Prodno
-	,p.descr as Beskrivelse
-    ,CAST(s.bal + s.stcinc - ino + cino as decimal(7,1)) as 'm2'
+	p.ProdNo as 'Prodno'
+	,p.descr as 'Beskrivelse'
+    ,CAST(sum(s.bal + s.stcinc - ino + cino) as decimal(7,1)) as 'm2'
 	,CAST(p.Free1 AS DECIMAL(7,1)) as 'Prpall'
-	,CAST((s.bal + s.stcinc - ino + cino)/NULLIF(p.free1, 0) as DECIMAL(7,0)) as  'Paller'
+	,CAST((sum(s.bal + s.stcinc - ino + cino)/NULLIF(p.free1, 0)) as DECIMAL(7,0)) as  'Paller'
   FROM F0015.dbo.StcBal s, f0015.dbo.prod p
   where 
   s.prodno = p.prodno
   and p.ProdPrGr <> 99
   and s.StcNo = 1
   and s.prodno <> 'adm'
-  
-	order by p.Descr
-	for json path
+
+  group by
+  p.ProdNo
+	,p.descr
+	,p.Free1
+   
+	order by p.Descr, p.ProdNo
+	for json auto
