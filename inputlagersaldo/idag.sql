3ebebecf4c0227a843ae969770ffc0c76bacc147
gruffsy <peamoen@gmail.com>
2022-12-07 20:37:03 +0100

    made changes

--- inputlagersaldo/idag.sql
+++ inputlagersaldo/idag.sql
@@ -15,7 +15,7 @@ Klient,
 
 sum(m_mva) AS 'mmoms',
 sum(u_mva) AS 'umoms',
-	sum(u_mva - kostnad AS 'db',
+	sum(u_mva - kostnad) AS 'db',
 	sum(u_mva - kostnad)/sum(u_mva) as 'dg',
 	count(distinct Ordrenummer) as 'antord',
 	sum(m_mva)/count(distinct Ordrenummer) as 'prord'
