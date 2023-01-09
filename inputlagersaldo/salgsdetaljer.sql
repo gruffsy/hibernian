af68d2744ef04dd2ec390440c8705c4966472594
gruffsy <peamoen@gmail.com>
2023-01-09 12:19:47 +0100

    made changes

---
+++ inputlagersaldo/salgsdetaljer.sql
@@ -0,0 +1,41 @@
+set
+    nocount on
+set
+    noexec on
+SET
+    LANGUAGE Norwegian
+set
+    noexec off
+
+	
+SELECT 
+	
+  butikk,
+	Navnr AS ItemNo,
+ferdigmeldtdato,
+sum(antall) as 'antall',
+sum(m_mva) AS 'mmoms',
+sum(u_mva) AS 'umoms',
+sum(u_mva - kostnad) AS 'db',
+CASE WHEN Ordretype = 1
+               THEN sum(m_mva)
+          END AS Kreditt
+
+	
+
+
+from f0001.dbo.PRODUKTRANSER_ALLE
+where 
+transaksjonstype = 1
+and antall <> 0
+and produktnr <> 'a1'
+and Kundeprisgruppe <> 4
+and ferdigmeldtdato >= 20220101
+
+group by
+butikk,
+navnr,
+ferdigmeldtdato,
+ordretype
+	
+
