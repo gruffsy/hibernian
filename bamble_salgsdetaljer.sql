de5ac83867f8d36cf9aa7dc10eb659f224923637
gruffsy <peamoen@gmail.com>
2023-01-09 12:32:19 +0100

    made changes

---
+++ bamble_salgsdetaljer.sql
@@ -0,0 +1,37 @@
+set
+    nocount on
+set
+    noexec on
+SET
+    LANGUAGE Norwegian;
+set
+    noexec off
+
+select
+    'Bamble' as butikk,
+    convert(varchar, th.[Date], 112) as Dato, 
+    [Item No_] as ItemNo,
+    SUM(Quantity)*-1 as 'antall',
+    sum([Total Rounded Amt_])*-1 as mmoms,
+    sum(se.[Net Amount])*-1 as umoms,
+    sum(se.[Net Amount])*-1-sum(se.[Cost Amount])*-1 as db,
+     [Customer Account] as Kreditt
+from
+    
+    [Megaflis Bamble AS$Trans_ Sales Entry] se
+    inner join [Megaflis Bamble AS$Transaction Header] th
+    on 
+       th.[Store No_]=se.[Store No_] and
+        th.[POS Terminal No_]=se.[POS Terminal No_] and
+        th.[Transaction No_]=se.[Transaction No_]
+    INNER JOIN [Megaflis Bamble AS$Store] s
+    on s.No_=th.[Store No_]
+where th.[Transaction Type]=2 
+group by 
+       [Item No_],
+       th.[Date],
+    [Customer Account]
+
+go
+quit
+
