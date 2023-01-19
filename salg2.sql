7e5568722f91a0a01edd32d12eda65a15353264f
gruffsy <peamoen@gmail.com>
2023-01-19 13:17:03 +0100

    made changes

--- salg2.sql
+++ salg2.sql
@@ -15,7 +15,7 @@ select
     sum(se.[Net Amount])*-1 as umoms,
     sum(se.[Net Amount])*-1-sum(se.[Cost Amount])*-1 as db,
 	count(distinct th.[Receipt No_]) as antord,
-    ([Customer Account])/1,25 as kreditt
+    [Customer Account] as kreditt
 from
       	[Megaflis Bamble AS$Trans_ Sales Entry] se 
 inner join [Megaflis Bamble AS$Transaction Header] th 
@@ -40,8 +40,8 @@ and (
         c.[Customer Price Group] <> 'INTERNT'
         )
 group by
-	th.[Date]
-    --[Customer Account]
+	th.[Date],
+    [Customer Account]
 order by
 	th.[Date]
 
