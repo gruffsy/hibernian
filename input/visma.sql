7cbf8b904ced559b5a3c941558724ef385f87050
gruffsy <peamoen@gmail.com>
2023-03-16 11:32:24 +0100

    made changes

---
+++ input/visma.sql
@@ -0,0 +1,36 @@
+set
+    nocount on
+set
+    noexec on
+SET
+    LANGUAGE Norwegian
+set
+    noexec off
+
+	
+select 
+    butikk,
+    Klient,
+    sum(m_mva) AS 'mmoms',
+    sum(u_mva) AS 'umoms',
+    sum(u_mva - kostnad) AS 'db',
+    sum(u_mva - kostnad)/sum(u_mva) as 'dg',
+    count(distinct Ordrenummer) as 'antord',
+    sum(m_mva)/count(distinct Ordrenummer) as 'prord'
+from 
+    f0001.dbo.PRODUKTRANSER_ALLE
+where 
+    fakturadato = convert(varchar, getdate(), 112)
+    and Fakturadato <> 0
+    and transaksjonstype = 1
+    and Ordretype = 3
+
+
+group by
+    butikk,
+    Klient
+
+order by
+    Klient
+	
+for json auto
