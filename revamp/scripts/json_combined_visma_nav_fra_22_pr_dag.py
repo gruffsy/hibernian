35eed1094e046e852c078bd995f337698bb557ce
gruffsy <peamoen@gmail.com>
2023-03-26 23:27:25 +0200

    made changes

---
+++ revamp/scripts/json_combined_visma_nav_fra_22_pr_dag.py
@@ -0,0 +1,72 @@
+import json
+from collections import defaultdict
+
+# Define a function to format a value as a currency string
+
+
+def format_currency(value):
+    return f"{int(value):,}".replace(",", " ") + " kr"
+
+
+def format_integer(value):
+    return f"{int(value):,}".replace(",", " ")
+
+
+# Combine data from File1.json and File2.json into a single list of dictionaries
+with open("../jsons/visma_salg_fra_22.json", "r", encoding="utf-8") as f:
+    file1_data = json.load(f)
+
+with open("../jsons/nav_salg_fra_22.json", "r", encoding="utf-8") as f:
+    file2_data = json.load(f)
+combined_data = file1_data + file2_data
+
+
+# Write the updated JSON string to CombinedFile.json
+with open("../jsons/salg_fra_22_pr_dag.json", "w", encoding="utf-8" ) as f:
+    json.dump(combined_data, f, indent=4, ensure_ascii=False)
+
+# Read sales.json
+with open("../jsons/salg_fra_22_pr_dag.json", "r", encoding="utf-8") as f:
+    data = json.load(f)
+
+# Sum sales for each date
+totals = defaultdict(lambda: {"mmoms": 0, "umoms": 0, "db": 0, "antord": 0})
+for row in data:
+    date = row["fakturadato"]
+    totals[date]["mmoms"] += row["mmoms"]
+    totals[date]["umoms"] += row["umoms"]
+    totals[date]["db"] += row["db"]
+    totals[date]["antord"] += row["antord"]
+
+# Add total rows to the existing data
+for date, total in totals.items():
+    new_row = {
+        "fakturadato": date,
+        "butikk": "Totalt",
+        "Klient": 99,  # set a higher Klient value so that it appears first when sorted
+        "mmoms": total["mmoms"],
+        "umoms": total["umoms"],
+        "db": total["db"],
+        "dg": total["db"] / total["umoms"] if total["umoms"] != 0 else 0,
+        "antord": total["antord"],
+        "prord": total["mmoms"] / total["antord"] if total["antord"] != 0 else 0,
+    }
+    data.append(new_row)
+
+
+# Sort the data by fakturadato ascending and Klient descending
+data.sort(key=lambda x: (-x["fakturadato"], int(x["Klient"])))
+
+
+for d in data:
+    # Convert values to a currency string
+    d["mmoms"] = format_currency(d["mmoms"])
+    d["umoms"] = format_currency(d["umoms"])
+    d["db"] = format_currency(d["db"])
+    d["prord"] = format_currency(d["prord"])
+    d["antord"] = format_integer(d["antord"])
+    d["dg"] = "{:.1%}".format(d["dg"])
+
+# Write the result to a new JSON file
+with open("../publish/salg_fra_22_pr_dag_med_total.json", "w", encoding="utf-8") as f:
+    json.dump(data, f, indent=4, ensure_ascii=False)
---
+++ revamp/scripts/sql2json_visma_salg_fra_22_pr_dag.py
@@ -0,0 +1,61 @@
+import pyodbc
+import json
+
+# Replace with your actual SQL Server details
+server = "DB-HIB"
+# database = "<database>"
+username = "sa"
+password = "VismaVudAdmin123@"
+# table = "<table>"
+
+# Connect to the SQL Server
+connection_string = f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};UID={username};PWD={password}"
+conn = pyodbc.connect(connection_string)
+
+# Run the SELECT clause
+query = '''
+SELECT 
+  fakturadato,
+  butikk,
+  Klient,
+  CAST(sum(m_mva) AS INT) AS 'mmoms',
+  CAST(sum(u_mva) AS INT) AS 'umoms',
+  CAST(sum(u_mva - kostnad) AS INT) AS 'db',
+  CAST(ROUND(sum(u_mva - kostnad)/sum(u_mva),2) AS FLOAT) as 'dg',
+  count(distinct Ordrenummer) as 'antord',
+  CAST((sum(m_mva)/count(distinct Ordrenummer)) AS INT) as 'prord'
+FROM f0001.dbo.PRODUKTRANSER_ALLE
+WHERE 
+  fakturadato BETWEEN '20220101' AND convert(varchar, getdate(), 112)
+  AND Fakturadato <> 0
+  AND transaksjonstype = 1
+  AND Ordretype = 3
+GROUP BY
+  fakturadato,
+  butikk,
+  Klient 
+order by
+fakturadato,
+klient
+
+'''
+
+
+cursor = conn.cursor()
+cursor.execute(query)
+
+# Fetch all rows and column names
+rows = cursor.fetchall()
+column_names = [column[0] for column in cursor.description]
+
+# Convert rows to a list of dictionaries
+result = [dict(zip(column_names, row)) for row in rows]
+
+# Save the result as JSON
+with open("../jsons/visma_salg_fra_22.json", "w", encoding="utf-8") as output_file:
+    json.dump(result, output_file, default=str, indent=4, ensure_ascii=False)
+
+
+# Close the connection
+cursor.close()
+conn.close()
