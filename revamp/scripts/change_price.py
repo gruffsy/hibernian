166e1aaf2e86422d80ea96aa86c86a69ca84c6d1
gruffsy <peamoen@gmail.com>
2023-08-01 13:31:12 +0200

    made changes

--- revamp/scripts/change_price.py
+++ revamp/scripts/change_price.py
@@ -1,74 +1,62 @@
 import pyodbc
 
-# Replace with your actual SQL Server details
+# Sett opp tilkoblingsdetaljer for Tabell NAV
 server_nav = "10.0.10.41"
-# database = "<database>"
 username_nav = "intranett"
 password_nav = "Megareader18"
-# table = "<table>"
 
-# Opprett tilkoblinger til databasene
+# Opprett tilkobling til NAV-databasen
 connection_string_nav = f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server_nav};UID={username_nav};PWD={password_nav}"
 conn_nav = pyodbc.connect(connection_string_nav)
 
-
-# Replace with your actual SQL Server details
+# Sett opp tilkoblingsdetaljer for Tabell Visma
 server_visma = "DB-HIB"
-# database = "<database>"
 username_visma = "sa"
 password_visma = "VismaVudAdmin123@"
-# table = "<table>"
 
-# Opprett tilkoblinger til databasene
+# Opprett tilkobling til Visma-databasen
 connection_string_visma = f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server_visma};UID={username_visma};PWD={password_visma}"
 conn_visma = pyodbc.connect(connection_string_visma)
 
 cursor_nav = conn_nav.cursor()
 cursor_visma = conn_visma.cursor()
 
-# Hent data fra Tabell NAV
+# Definer SQL-spørringen for å hente data fra Tabell NAV
 query_nav = '''
-select 
-        top 10000
-		No_ AS Nr,
-		[Unit Cost] AS Kostpris,
-		[Unit Price Including VAT] AS Normalpris
-		--[Date Created] AS [Opprettet dato],
-		-- [Last Date Modified] AS [Endret dato]
-                
-
-    FROM            MegaFlisMASTER$Item
-    -- where No_ = '006029'
-    order by [Last Date Modified] desc
-
-
-
+SELECT TOP 100
+    No_ AS Nr,
+    [Unit Cost] AS Kostpris,
+    [Unit Price Including VAT] AS Normalpris
+FROM MegaFlisMASTER$Item
+ORDER BY [Last Date Modified] DESC
 '''
-
-
 
 cursor_nav.execute(query_nav)
 rows_nav = cursor_nav.fetchall()
-# print(rows_nav)
 
-
+# For hver rad i resultatet fra Tabell NAV
 for row in rows_nav:
     Nr, Kostpris, Normalpris = row
-    # print(row)
-    # Hent den aktuelle raden fra Tabell Visma
+
+    # Hent den tilsvarende raden fra Tabell Visma basert på Nr/TrInf3
     cursor_visma.execute("SELECT TrInf3, Free3, Free4 FROM [F0001].[dbo].Prod WHERE TrInf3 = ?", Nr)
     row_visma = cursor_visma.fetchone()
-    # print(row_visma)
 
-    # Sjekk om det er forskjeller, og oppdater raden hvis det er det
-    if row_visma is not None and (row_visma[2] != Normalpris):
+    # Sjekk om det er forskjeller mellom de to radene
+    if row_visma is not None and (row_visma[1] != Kostpris or row_visma[2] != Normalpris):
+        # Hvis det er forskjeller, oppdater raden i Tabell Visma med de nye verdiene
         cursor_visma.execute("""
             UPDATE [F0001].[dbo].Prod 
             SET Free3 = ?, Free4 = ? 
             WHERE TrInf3 = ?
         """, Kostpris, Normalpris, Nr)
         conn_visma.commit()
+
+        # Hent den oppdaterte raden fra Tabell Visma for å bekrefte endringene
         cursor_visma.execute("SELECT TrInf3, Free3, Free4 FROM [F0001].[dbo].Prod WHERE TrInf3 = ?", Nr)
         row_visma = cursor_visma.fetchone()
+
+        # Skriv ut den opprinnelige raden fra Tabell NAV og den oppdaterte raden fra Tabell Visma
         print(row)
         print(row_visma)
+
