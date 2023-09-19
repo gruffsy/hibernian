efb04b06af3b0594575ca24a7caf65779ea974a8
gruffsy <peamoen@gmail.com>
2023-09-19 15:01:30 +0200

    Updated background files

--- revamp/scripts/item.py
+++ revamp/scripts/item.py
@@ -2,86 +2,65 @@ import pyodbc
 import csv
 
 # Replace with your actual SQL Server details
-server = "10.0.10.41"
-# database = "<database>"
-username = "intranett"
-password = "Megareader18"
+server = "mf-ls-sql02.norwayeast.cloudapp.azure.com"
+database = "Megaflis_AS"
+username = "perarne"
+password = "AdaiQQvlq!#to43"
 # table = "<table>"
 
 
 # Connect to the SQL Server
-connection_string = f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};UID={username};PWD={password}"
+connection_string = f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};Database={database};UID={username};PWD={password}"
 conn = pyodbc.connect(connection_string)
 
 # Run the SELECT clause
 query = """
-with 
-    items as (
-        select 
-            No_,
-            [Description 3],
-            [Sales Unit of Measure], 
-            [Vendor Item No_], 
-            GTIN, 
-            [Item Family Code], 
-            [Purchaser Code], 
-            [Product Group Code], 
-            [Item Category Code], 
-            [Vendor No_], 
-            [Dangerous chemicals], 
-            [Attrib 2 Code], 
-            [Date Created]
-        from 
-            [MegaFlisMASTER$Item]
-            --where [No_] = '019441' --debugging
-        -- where [Exclude from Replenishment] = 0
-    ),
-    categories as (
-        select 
-            ic.Code as [Varekategorikode], 
-            ic.Description as [Varekategori], 
-            pg.Code as [Produktgruppekode], 
-            pg.Description as [Produktgruppe]
-        from 
-            [MegaFlisMASTER$Product Group] pg 
-            inner join [MegaFlisMASTER$Item Category] ic on pg.[Item Category Code] = ic.Code),
-            staff as (select Code, Name
-            from [MegaFlisMASTER$Salesperson_Purchaser]),
-            brands as (select Code, Description
-            from [MegaFlisMASTER$Item Family]
-    ),
-    vendors as (
-        select 
-            No_, 
-            Name
+SELECT
+    i.No_,
+    i.[Vendor Item No_],
+    i.GTIN,
+    Item2.[Description 3],
+    i.[Sales Unit of Measure],
+    LSCItem.[LSC Attrib 2 Code] AS 'Attrib 2 Code',
+    Item2.[Dangerous chemicals],
+    i2.[LSC Date Created] AS [Date Created],
+    Vendor.Name as [Leverandør],
+    staff.Name as [Innkjøper],
+    ItemCategory.Description AS Varekategori,
+    ProductGroup.Description AS Produktgruppe,
+    brand.[Description] as [Brand]
 
-        from 
-            [MegaFlisMASTER$Vendor]
-    )
-    select 
-        items.No_, 
-        items.[Vendor Item No_],
-        items.GTIN, 
-        items.[Description 3], 
-        items.[Sales Unit of Measure], 
-        items.[Attrib 2 Code], 
-        items.[Dangerous chemicals], 
-        items.[Date Created],
-        vendors.Name as [Leverandør],
-        staff.Name as [Innkjøper],
-        categories.Varekategori, 
-        categories.Produktgruppe,
-        brands.Description as [Brand]
-    from 
-        items
-            inner join 
-                categories on categories.Produktgruppekode = items.[Product Group Code] 
-                and categories.Varekategorikode = items.[Item Category Code]
-            left outer join 
-                staff on items.[Purchaser Code] = staff.[Code]
-            left outer join 
-            brands on brands.Code = items.[Item Family Code]
-            left outer join vendors on vendors.No_ = items.[Vendor No_]
+-- LSCItem.[LSC Item Family Code] AS 'Item Family Code',
+-- Item2.[Purchaser Code],
+-- ProductGroup.Code AS 'Product Group Code',
+-- ItemCategory.Code AS 'Item Category Code',
+-- Vendor.No_,
+-- i.[Unit Cost] AS Kostpris,
+-- LSCItem.[LSC Unit Price Incl_ VAT] AS Normalpris,
+-- i.[Order Multiple] AS [Kolli str],
+-- ProductGroup.Description AS Varegruppe,
+-- ItemCategory.Description AS Hovedgruppe,
+-- Item2.[Web Item] AS WebItem,
+-- i.[$systemCreatedAt] AS [Opprettet dato],
+-- i.[$systemModifiedAt] AS [Endret dato]
+FROM
+    [MASTER$Item$437dbf0e-84ff-417a-965d-ed2bb9650972] AS i
+    inner join [MASTER$Item$5ecfc871-5d82-43f1-9c54-59685e82318d] i2 on i.No_ = i2.No_
+    inner JOIN
+    [MASTER$Item Category$437dbf0e-84ff-417a-965d-ed2bb9650972] AS ItemCategory ON ItemCategory.Code = i.[Item Category Code]
+    inner JOIN
+    [MASTER$LSC Retail Product Group$5ecfc871-5d82-43f1-9c54-59685e82318d] AS ProductGroup ON ProductGroup.Code = i2.[LSC Retail Product Code]
+        AND ItemCategory.Code = ProductGroup.[Item Category Code]
+    INNER JOIN
+    [MASTER$Vendor$437dbf0e-84ff-417a-965d-ed2bb9650972] AS Vendor ON Vendor.No_ = i.[Vendor No_]
+    INNER JOIN
+    [MASTER$Item$64848631-618b-42d9-91c4-5fffcbea6f69] AS Item2 on Item2.No_ = i.No_
+    INNER JOIN
+    [MASTER$Item$5ecfc871-5d82-43f1-9c54-59685e82318d] AS LSCItem ON LSCItem.No_ = i.No_
+    INNER JOIN
+    [Megaflis AS$Salesperson_Purchaser$437dbf0e-84ff-417a-965d-ed2bb9650972] staff ON staff.Code=Item2.[Purchaser Code]
+    LEFT JOIN
+    [MASTER$LSC Item Family$5ecfc871-5d82-43f1-9c54-59685e82318d] brand ON LSCItem.[LSC Item Family Code] = [brand].Code
 
 """
 
