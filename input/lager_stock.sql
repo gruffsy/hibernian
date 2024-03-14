55c87bc8d85a91348995a182812c4ce36fae8dfa
gruffsy <peamoen@gmail.com>
2024-03-14 13:47:02 +0100

    Updated background files

--- input/lager_stock.sql
+++ input/lager_stock.sql
@@ -10,8 +10,8 @@ select (
 SELECT 
 	p.ProdNo as 'Prodno'
 	,p.descr as 'Beskrivelse'
-    ,CAST(sum(s.bal + s.stcinc - ino + cino) as decimal(7,1)) as 'm2 på lager'
-	,CAST(p.Free1 AS DECIMAL(7,1)) as 'm2 pr pall'
+    ,CAST(sum(s.bal + s.stcinc - ino + cino) as decimal(7,1)) as 'antall på lager'
+	,CAST(p.Free1 AS DECIMAL(7,1)) as 'antall pr pall'
 	,CAST((sum(s.bal + s.stcinc - ino + cino)/NULLIF(p.free1, 0)) as DECIMAL(7,0)) as  'Paller på lager'
     ,CAST(sum(s.InPurc/NULLIF(p.free1, 0)) as DECIMAL(7,0)) as 'Paller på vei'
   FROM F0015.dbo.StcBal s, f0015.dbo.prod p
